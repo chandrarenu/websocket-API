df97a8f681e1a9556bdd8cf3a28c1581e40ea079
chandrarenu <chandrarenu07@gmail.com>
2024-05-02 12:44:00 +0545

    initial work

--- attendance_project/attendance/consumers.py
+++ attendance_project/attendance/consumers.py
@@ -3,11 +3,21 @@ import json
 
 class AttendanceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        print(self.scope)
+        print("-----------------------------")
+        # self.scope['session'].id
+        
+    
         await self.accept()
+        
+        print("connection established")
+
 
     async def disconnect(self, close_code):
         pass
 
     async def receive(self, text_data):
-        attendance_data = json.loads(text_data)
+        print(text_data)
+        # attendance_data = json.loads(text_data)
         
+    
